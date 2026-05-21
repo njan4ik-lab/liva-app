48df35c78f5f899a5f5e00f624784f3968f76354
njan4ik-lab <njan4ik@gmail.com>
2026-05-21 21:57:40 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'liva-v1';
+const FILES = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
